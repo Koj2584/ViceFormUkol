06ede075575060af26481dc99e025c44760bae19
Koj2584 <73990215+Koj2584@users.noreply.github.com>
2023-10-09 13:09:58 +0200

    Add files via upload

---
+++ ViceForm/ViceForm/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ViceForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Form form2 = new Form();
+        Random r = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            form2.Show();
+            this.Visible = false;
+            int x = r.Next(0, 1800);
+            int y = r.Next(0, 1000);
+            form2.Location = new Point(x, y);
+            form2.MouseEnter += uskoc;
+            form2.FormBorderStyle = FormBorderStyle.None;
+            Timer t = new Timer();
+            t.Interval = 100;
+            t.Tick += T_Tick;
+            //t.Start();
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            Form f =new Form();
+            f.Show();
+            int x = r.Next(0, 1800);
+            int y = r.Next(0, 1000);
+            f.Location = new Point(x, y);
+            f.MouseEnter += uskoc;
+            f.FormBorderStyle = FormBorderStyle.None;
+        }
+
+        private void uskoc(object sender, EventArgs e)
+        {
+            int x = r.Next(0, 1800);
+            int y = r.Next(0, 1000);
+            ((Form)sender).Location = new Point(x, y);
+            Form f = new Form();
+            f.Show();
+            x = r.Next(0, 1800);
+            y = r.Next(0, 1000);
+            f.Location = new Point(x, y);
+            f.MouseEnter += uskoc;
+            f.FormBorderStyle = FormBorderStyle.None;
+        }
+    }
+}
---
+++ ViceForm/ViceForm/Form2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ViceForm
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            this.AcceptButton = button1;
+        }
+        Form form2;
+        List<string> l = new List<string>();
+        string text = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            form2 = new Form();
+            form2.Width = 500;
+            Button b1 = new Button();
+            b1.Text = "Ulož";
+            b1.Click += B1_Click;
+            b1.Location = new Point(0, 100);
+            Button b2 = new Button();
+            b2.Text = "Zruš";
+            b2.Click += B2_Click;
+            b2.Location = new Point(200, 100);
+            TextBox txt = new TextBox();
+            txt.Location = new Point(0, 0);
+            txt.TextChanged += Txt_TextChanged;
+
+
+            form2.Controls.Add(txt);
+            form2.Controls.Add(b1);
+            form2.Controls.Add(b2);
+            form2.Focus();
+            form2.Show();
+            form2.ActiveControl = txt;
+            form2.AcceptButton = b1;
+        }
+
+        private void Txt_TextChanged(object sender, EventArgs e)
+        {
+            text = ((TextBox)sender).Text;
+        }
+
+        private void B2_Click(object sender, EventArgs e)
+        {
+            form2.Close();
+            text = "";
+        }
+
+        private void B1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(text);
+            form2.Close();
+            text = "";
+        }
+    }
+}
